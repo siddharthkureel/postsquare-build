4ca120750361e4bbd298df58b2641c2a82b8f646
joykureel <43037412+joykureel@users.noreply.github.com>
2019-08-15 10:55:10 +1200

    first commit

---
+++ precache-manifest.a40d09b7fbbedb74f9ef0bd0a49f63c7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "be1e854233f44a218100",
+    "url": "./static/js/main.1263ab6e.chunk.js"
+  },
+  {
+    "revision": "057fb3bc81d2aff7adaf",
+    "url": "./static/js/2.acc300e0.chunk.js"
+  },
+  {
+    "revision": "1b22b914b13c62f0c8f48c571321069e",
+    "url": "./index.html"
+  }
+];
